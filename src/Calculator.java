40686d30959afcb63c4c40da593d2d69686dd331
Alexey Petin <alex_beholder@mail.ru>
2023-01-18 22:10:02 +0300

    Fix "divide" method

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,22 +3,16 @@ import java.util.function.*;
 public class Calculator {
     public static Supplier<Calculator> instance = Calculator::new;
 
-    BinaryOperator<Integer> plus = (x, y) -> x + y;
-    BinaryOperator<Integer> minus = (x, y) -> x - y;
-    BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> {
-        try {
-            return x / y;
-        } catch (ArithmeticException e) {
-            System.out.println("Деление на ноль");
-            return 0;
-        }
-    };
-
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
     Predicate<Integer> isPositive = x -> x > 0;
 
     Consumer<Integer> println = System.out::println;
+    BinaryOperator<Integer> plus = Integer::sum;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        return x == 0 ? 0 : x / y;
+    };
 }
